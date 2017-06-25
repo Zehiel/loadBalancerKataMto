be52f1dde834d32c97d6052776ba13580cbafa55
Andrzej Borzecki <andrew.borzecki@gmail.com>
2017-06-25 16:26:36 +0200

    Balancing server with no vms

---
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -0,0 +1,10 @@
+package edu.iis.mto.serverloadbalancer;
+
+/**
+ * Created by grusz on 25.06.2017.
+ */
+public class ServerLoadBalancer {
+    public void balance(Server[] servers, Vm[] vms) {
+
+    }
+}
